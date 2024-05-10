e82f955c0c1b8a60251ab1f311d170f29a0e4416
emracatal <134175723+emracatal@users.noreply.github.com>
2024-05-10 18:08:18 +0300

    bilisimvadisi.com.tr/hakkimizda/firmalar eklendi

---
+++ src/test/java/bilisimVadisiCompanies/Case1.java
@@ -0,0 +1,28 @@
+package bilisimVadisiCompanies;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Case1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bilisimvadisi.com.tr/hakkimizda/firmalar/");
+        //<a href="javascript:void(0)" class="loadmore-exbt" data-id="extp-8948"> <span class="load-text">Load more</span><span></span>&nbsp;<span></span>&nbsp;<span></span> </a>
+        WebElement loadMoreButton = driver.findElement(By.xpath("//button[contains(text(),'Daha fazlasını yükle')]"));
+        loadMoreButton.click();
+        List<WebElement> linkList = driver.findElements(By.tagName("a"));
+        System.out.println();
+        for (WebElement each : linkList) {
+            String linkText = each.getAttribute("href");
+            System.out.println(linkText);
+        }
+        driver.close();
+
+    }
+}
